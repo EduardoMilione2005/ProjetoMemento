36f2cc62f5e41be6eb5bad450e1bdc0ae381e575
Eduardo Gomes Milione <eduardo.milione8@gmail.com>
2026-05-14 11:41:10 -0300

    Criando o arquivo TVMemento.java

---
+++ src/main/java/TVMemento.java
@@ -0,0 +1,18 @@
+public class TVMemento {
+
+    private final int canal;
+    private final int volume;
+
+    public TVMemento(int canal, int volume) {
+        this.canal = canal;
+        this.volume = volume;
+    }
+
+    public int getCanal() {
+        return canal;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+}
